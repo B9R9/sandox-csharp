88880587b5dfe5779c990938925a91e438047906
B9R9 <baptiste.riffard@gmail.com>
2023-08-21 09:31:48 +0300

    Add money Maker

---
+++ MoneyMaker/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MoneyMaker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double  userAmount;
+            int     goldValue;
+            int     silverValue;
+            int goldCoin;
+            int silverCoin;
+            int remainder;
+
+            goldValue = 10;
+            silverValue = 5;
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("Enter an amount to convert to coins?");
+            userAmount = Math.Floor(Convert.ToDouble(Console.ReadLine()));
+
+            goldCoin = (int)Math.Floor(userAmount / goldValue);
+            remainder = (int)userAmount % goldValue;
+            silverCoin = (int)Math.Floor((double)remainder / silverValue);
+            remainder = (int)remainder % silverValue;
+            Console.WriteLine($"{userAmount} in coins is equal to {goldCoin}" +
+            $" gold coins, {silverCoin} silver coins and {remainder} bronze coins!");
+            Console.WriteLine("Press Enter to quit ...");
+            Console.ReadLine();
+
+        }
+    }
+}
